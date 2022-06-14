d542629663d3652961a98873f79d0d43bd25dab5
wnsxor1993 <wnsxor1993@naver.com>
2022-06-14 17:22:43 +0900

    [wnsxor1993#3]feat: LoginInputView 생성 및 addSubview 연결

---
+++ iOS/IssueTracker/IssueTracker/LoginInputView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginInputView.swift
+//  IssueTracker
+//
+//  Created by juntaek.oh on 2022/06/14.
+//
+
+import UIKit
+
+class LoginInputView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS/IssueTracker/IssueTracker/UIView+.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+.swift
+//  IssueTracker
+//
+//  Created by juntaek.oh on 2022/06/14.
+//
+
+import Foundation
