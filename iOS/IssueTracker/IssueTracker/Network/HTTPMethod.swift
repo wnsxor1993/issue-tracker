39bc14c8aba8cda324d4f0d34063f63c7523b194
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-06-14 16:44:58 +0900

    [wnsxor1993#4] feat: NetworkService, network Error 구현

---
+++ iOS/IssueTracker/IssueTracker/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  IssueTracker
+//
+//  Created by Kai Kim on 2022/06/14.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PATCH
+    case DELETE
+}
