39c56adebd90031e93272ca4db4a08ca077b2504
wnsxor1993 <wnsxor1993@naver.com>
2022-06-15 14:41:00 +0900

    [wnsxor1993#6]feat: OAuthButton action delegate 구현 및 action 연결

--- iOS/IssueTracker/IssueTracker/LoginScene/View/OAuthLoginView.swift
+++ iOS/IssueTracker/IssueTracker/LoginScene/View/OAuthLoginView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class OAuthLoginView: UIView {
     
+    var delegate: OAuthButtonDelegate?
+    
     private let githubLoginButton: OAuthLoginButton = {
         let button = OAuthLoginButton()
         button.setImageWithTitle(imageName: .gitImage, title: .gitTitle)
@@ -51,5 +53,24 @@ private extension OAuthLoginView {
             githubLoginButton.bottomAnchor.constraint(equalTo: appleLoginButton.topAnchor, constant: -14),
             githubLoginButton.heightAnchor.constraint(equalToConstant: 56)
         ])
+    }
+    
+    func setOAuthButtonAction() {
+        let uiAction = UIAction(handler: { _ in
+            self.delegate?.didClickButton()
+        })
+        
+        if #available(iOS 14.0, *) {
+            appleLoginButton.addAction(uiAction, for: .touchDown)
+            githubLoginButton.addAction(uiAction, for: .touchDown)
+        } else {
+            appleLoginButton.addTarget(self, action: #selector(touchedOAuthButton), for: .touchDown)
+            githubLoginButton.addTarget(self, action: #selector(touchedOAuthButton), for: .touchDown)
+        }
+    }
+    
+    @objc
+    func touchedOAuthButton() {
+        self.delegate?.didClickButton()
     }
 }
