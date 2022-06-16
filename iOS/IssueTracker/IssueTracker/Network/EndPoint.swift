5079f5756a406282a87201430e01911fea48174a
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-06-16 15:59:32 +0900

    [wnsxor1993#20] refactor: Endpoint ClientID, ClientSecrete 의 정보를 plist 에서 가져오는 부분 분리

--- iOS/IssueTracker/IssueTracker/Network/EndPoint.swift
+++ iOS/IssueTracker/IssueTracker/Network/EndPoint.swift
@@ -10,8 +10,19 @@ import Foundation
 protocol Authenticatable {
     static var host: String {get}
     static var path: String {get}
-    static var clientID: String {get}
-    static var clientSecrete: String {get}
+    static var clientID: String? {get}
+    static var clientSecrete: String? {get}
+
+}
+
+extension Bundle {
+
+    static func searchObject(from plist: String, key: String) -> String? {
+        guard let filePath = main.path(forResource: plist, ofType: "plist") else {return nil}
+        let plist = NSDictionary(contentsOfFile: filePath)
+        let value = plist?.object(forKey: "gitClient_ID") as? String
+        return  value == nil ? nil : value
+    }
 
 }
 
@@ -21,37 +32,28 @@ enum GitAuthentication: Authenticatable {
 
     static var path = "/login/oauth/authorize"
 
-    static var clientID: String {
-        guard let filePath = Bundle.main.path(forResource: "ClientKey", ofType: "plist") else {return
-            ""}
-
-        let plist = NSDictionary(contentsOfFile: filePath)
-
-        guard let value = plist?.object(forKey: "gitClient_ID") as? String else {return
-            ""}
-        return value
+    static var clientID: String? {
+        guard let clientID = Bundle.searchObject(from: "ClientKey", key: "gitClient_ID") else {return nil}
+        return clientID
     }
 
-    static var clientSecrete: String {
-        guard let filePath = Bundle.main.path(forResource: "ClientKey", ofType: "plist") else {return
-            ""}
-
-        let plist = NSDictionary(contentsOfFile: filePath)
-
-        guard let value = plist?.object(forKey: "gitClient_Secrete") as? String else {return
-            ""}
-        return value
+    static var clientSecrete: String? {
+        guard let clientSecrete = Bundle.searchObject(from: "ClientKey", key: "gitClient_Secrete") else {return nil}
+        return clientSecrete
     }
 }
 
 protocol EndPoint {
-    var queryItems: [URLQueryItem] {get}
+    var queryItems: [URLQueryItem]? {get}
     var url: URL {get}
 }
 
 struct OauthEndPoint<Authentication: Authenticatable>: EndPoint {
 
-    var queryItems: [URLQueryItem] = [URLQueryItem(name: "client_id", value: "\(Authentication.clientID)")]
+    var queryItems: [URLQueryItem]? {
+        guard let clientID =  Authentication.clientID else {return nil}
+        return [URLQueryItem(name: "client_id", value: "\(clientID)")]
+    }
     var url: URL {
         var components = URLComponents()
         components.scheme = "https"
