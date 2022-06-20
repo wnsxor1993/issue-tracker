5a95834997b295b49867599d87af9803ff627b15
hwicode <khc3796@naver.com>
2022-06-20 17:32:08 +0900

    [wnsxor1993#33] feat: 이슈 수정 요청, 응답 dto 생성

---
+++ BE/src/main/java/com/codesquad/issuetracker/web/dto/IssueUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.codesquad.issuetracker.web.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class IssueUpdateRequest {
+
+    private String title;
+    private String content;
+    private Long milestoneId;
+    private List<Long> labelIds = new ArrayList<>();
+    private List<Long> assigneeIds = new ArrayList<>();
+}
