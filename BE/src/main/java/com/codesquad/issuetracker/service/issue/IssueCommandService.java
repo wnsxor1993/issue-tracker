807b1958504477d943429011142e5942a470799f
ttasjwi <ttasjwi920@gmail.com>
2022-06-20 23:51:00 +0900

    [wnsxor1993#44] feat: IssueCommandService에서 이슈생성 기능 구현

--- BE/src/main/java/com/codesquad/issuetracker/service/issue/IssueCommandService.java
+++ BE/src/main/java/com/codesquad/issuetracker/service/issue/IssueCommandService.java
@@ -1,10 +1,21 @@
 package com.codesquad.issuetracker.service.issue;
 
+import com.codesquad.issuetracker.domain.Issue;
+import com.codesquad.issuetracker.domain.Label;
+import com.codesquad.issuetracker.domain.Member;
+import com.codesquad.issuetracker.domain.Milestone;
+import com.codesquad.issuetracker.excption.MemberNotFoundException;
+import com.codesquad.issuetracker.excption.MilestoneNotFoundException;
 import com.codesquad.issuetracker.repository.issue.IssueRepository;
+import com.codesquad.issuetracker.repository.label.LabelRepository;
+import com.codesquad.issuetracker.repository.member.MemberRepository;
+import com.codesquad.issuetracker.repository.milestone.MilestoneRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,5 +24,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class IssueCommandService {
 
     private final IssueRepository issueRepository;
+    private final MemberRepository memberRepository;
+    private final MilestoneRepository milestoneRepository;
+    private final LabelRepository labelRepository;
+
+    /**
+     * 이슈 생성
+     */
+    public Long createIssue(Long authorId, List<Long> assigneeIds, Long milestondId, List<Long> labelIds, String title, String content) {
+        Member author = findMember(authorId);
+        List<Member> assigneeMembers = memberRepository.findAllById(assigneeIds);
+        Milestone milestone = findMilestone(milestondId);
+        List<Label> labels = labelRepository.findAllById(labelIds);
+
+        Issue issue = Issue.create(author, assigneeMembers, milestone,labels,title, content);
+        issueRepository.save(issue);
+        return issue.getId();
+    }
+
+    private Milestone findMilestone(Long milestoneId) {
+        return milestoneRepository.findById(milestoneId)
+                .orElseThrow(() -> new MilestoneNotFoundException("일치하는 식별자의 마일스톤이 존재하지 않습니다."));
+    }
+
+    private Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(()-> new MemberNotFoundException("일치하는 식별자의 회원이 존재하지 않습니다."));
+    }
 
 }
