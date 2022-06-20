35e14bd9a8e6922df15d0dcac47ddb837dd058ce
ttasjwi <ttasjwi920@gmail.com>
2022-06-20 23:25:14 +0900

    [wnsxor1993#40] feat: 이슈 생성 로직 추가
    - create 메서드를 통해서만 이슈를 생성하도록 함
    - IssueAssignee, IssueLabel의 생명주기는 Issue에 종속적이므로 package-private로 생성 메서드를 외부에서 호출하지 못 하도록 함.

--- BE/src/main/java/com/codesquad/issuetracker/domain/Issue.java
+++ BE/src/main/java/com/codesquad/issuetracker/domain/Issue.java
@@ -1,16 +1,17 @@
 package com.codesquad.issuetracker.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import static com.codesquad.issuetracker.domain.IssueAssignee.*;
+import static com.codesquad.issuetracker.domain.IssueLabel.*;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
@@ -31,14 +32,14 @@ public class Issue {
     private Member author;
 
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<IssueAssignee> assignees = new HashSet<>();
+    private List<IssueAssignee> assignees = new ArrayList<>();
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "milestone_id")
     private Milestone milestone;
 
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<IssueLabel> issueLabels = new HashSet<>();
+    private List<IssueLabel> issueLabels = new ArrayList<>();
 
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
@@ -48,12 +49,44 @@ public class Issue {
 
     private boolean isOpened;
 
-    public Issue(Member author, Set<IssueAssignee> assignees, Milestone milestone, String title, String content, boolean isOpened) {
+    @Builder(access = AccessLevel.PRIVATE)
+    private Issue(LocalDateTime createdAt, LocalDateTime lastModifiedAt, Member author, Milestone milestone,String title, String content, boolean isOpened) {
+        this.createdAt = createdAt;
+        this.lastModifiedAt = lastModifiedAt;
         this.author = author;
-        this.assignees = assignees;
         this.milestone = milestone;
         this.title = title;
         this.content = content;
         this.isOpened = isOpened;
     }
+
+    public static Issue create(Member author, List<Member> assigneeMembers, Milestone milestone, List<Label> labels, String title, String content) {
+
+        LocalDateTime createdTime = LocalDateTime.now();
+
+        Issue issue = Issue.builder()
+                .author(author)
+                .milestone(milestone)
+                .title(title)
+                .content(content)
+                .createdAt(createdTime)
+                .lastModifiedAt(createdTime)
+                .isOpened(true)
+                .build();
+
+        initAssigneeMembers(assigneeMembers, issue);
+        initLabels(labels, issue);
+        return issue;
+    }
+
+    private static void initAssigneeMembers(List<Member> assigneeMembers, Issue issue) {
+        assigneeMembers.stream()
+                .forEach(member -> assignMemberToIssue(member, issue));
+    }
+
+    private static void initLabels(List<Label> labels, Issue issue) {
+        labels.stream()
+                .forEach(label -> putLabelToIssue(label, issue));
+    }
+
 }
--- BE/src/main/java/com/codesquad/issuetracker/domain/IssueLabel.java
+++ BE/src/main/java/com/codesquad/issuetracker/domain/IssueLabel.java
@@ -24,8 +24,18 @@ public class IssueLabel {
     @JoinColumn(name = "label_id")
     private Label label;
 
-    public IssueLabel(Issue issue, Label label) {
+    private IssueLabel(Issue issue, Label label) {
         this.issue = issue;
         this.label = label;
     }
+
+    /**
+     * 이슈에 라벨을 할당하기 위해 사용하는 편의 메서드
+     * Issue에 종속적임. 생성 시 Issue에서만 생성하도록 하고 package-private로 함
+     */
+    static void putLabelToIssue(Label label, Issue issue) {
+        IssueLabel issueLabel = new IssueLabel(issue, label);
+        issue.getIssueLabels().add(issueLabel);
+    }
+
 }
