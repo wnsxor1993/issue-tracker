c33d89be0210b39639d41b861a3625e65b68a5fa
wnsxor1993 <wnsxor1993@naver.com>
2022-06-21 11:18:23 +0900

    [wnsxor1993#47]feat: IssueAddViewController 초기 생성

---
+++ iOS/IssueTracker/IssueTracker/IssueAddScene/IssueAddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IssueAddViewController.swift
+//  IssueTracker
+//
+//  Created by juntaek.oh on 2022/06/21.
+//
+
+import UIKit
+
+class IssueAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
