dbbcf7cd09148ba498206714ab412153d7519ba0
ttasjwi <ttasjwi920@gmail.com>
2022-06-23 23:17:01 +0900

    [wnsxor1993#77] fix: IssueRepository에 있던 사용자 정의 메서드를 사용자 정의 레포지토리로 이동
    - IssueRepository에 사용자정의 인터페이스를 구현해뒀는데 다시 보니 IssueCustomRepository에 정의해두지 않았음.
    - 이동함

--- BE/src/main/java/com/codesquad/issuetracker/repository/issue/IssueCustomRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/repository/issue/IssueCustomRepository.java
@@ -1,5 +1,10 @@
 package com.codesquad.issuetracker.repository.issue;
 
+import com.codesquad.issuetracker.domain.Issue;
+
+import java.util.Optional;
+
 public interface IssueCustomRepository {
 
+    Optional<Issue> findByIdWithAuthorAndMilestone(Long issueId);
 }
--- BE/src/main/java/com/codesquad/issuetracker/repository/issue/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/repository/issue/IssueRepository.java
@@ -3,9 +3,6 @@ package com.codesquad.issuetracker.repository.issue;
 import com.codesquad.issuetracker.domain.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface IssueRepository extends JpaRepository<Issue, Long>, IssueCustomRepository {
 
-    Optional<Issue> findByIdWithAuthorAndMilestone(Long issueId);
 }
