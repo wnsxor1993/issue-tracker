66857ef1bb6112cc3cd21b168dc92444b7277720
wnsxor1993 <wnsxor1993@naver.com>
2022-06-24 11:42:49 +0900

    feat: Observable 객체 구현

---
+++ iOS/IssueTracker/IssueTracker/Helper/Observable.swift
@@ -0,0 +1,32 @@
+//
+//  Observable.swift
+//  IssueTracker
+//
+//  Created by juntaek.oh on 2022/06/24.
+//
+
+import Foundation
+
+final class Observable<T> {
+    typealias Listener = ((T) -> Void)
+
+    private var listener: Listener?
+    private var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    init(_ value: T) {
+        self.value = value
+    }
+
+    func bind(listener: @escaping Listener) {
+        listener(value)
+        self.listener = listener
+    }
+
+    func updateValue(value: T) {
+        self.value = value
+    }
+}
