650bcbc4b546d779d0722402d3fda3bd6c3d5556
ttasjwi <ttasjwi920@gmail.com>
2022-07-01 11:24:48 +0900

    [wnsxor1993#109] feat: 이슈목록요청 Dto를 searchCondition으로 변경하는 메서드 추가

--- BE/src/main/java/com/codesquad/issuetracker/web/dto/issue/IssueListRequest.java
+++ BE/src/main/java/com/codesquad/issuetracker/web/dto/issue/IssueListRequest.java
@@ -1,15 +1,35 @@
 package com.codesquad.issuetracker.web.dto.issue;
 
-import lombok.Getter;
+import com.codesquad.issuetracker.dto.IssueSearchCondition;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Getter
-@ToString
+@RequiredArgsConstructor
+@ToString(of = {"isOpened", "authorId", "commentAuthorId"})
 public class IssueListRequest {
 
-    private final boolean isOpened;
+    private final Boolean isOpened;
+    private final Long authorId;
+    private final Long commentAuthorId;
 
-    public IssueListRequest(boolean isOpened) {
-        this.isOpened = isOpened;
+    public IssueSearchCondition toSearchCondition(IssueListRequest listRequest) {
+        return IssueSearchCondition.builder()
+                .isOpened(isOpened)
+                .authorId(authorId)
+                .commentAuthorId(commentAuthorId)
+                .build();
+    }
+
+    public Boolean getIsOpened() {
+        return isOpened;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public Long getCommentAuthorId() {
+        return commentAuthorId;
     }
 }
+
